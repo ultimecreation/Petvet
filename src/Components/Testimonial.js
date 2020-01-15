d2c2adb6509e60e68df227189e9c047a741805f6
ultimecreation <naserbechata@gmail.com>
2020-01-15 16:33:21 +0100

    added TestimonialContext throw Context hooks to testimonial

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,51 +1,8 @@
 import React from "react";
-
+import { Testi } from "../Context/TestimonialContext";
 const Testimonial = () => {
-  const slides = [
-    {
-      id: 1,
-      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit
-    excepturi ipsa harum magni labore porro voluptates quibusdam tempora
-    quas sequi animi? Accusantium hic laboriosam provident quaerat ullam
-    soluta reiciendis libero sit nobis? Quasi, sit?`,
-      author: "Robert T",
-      location: "Jura"
-    },
-    {
-      id: 2,
-      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit
-    excepturi ipsa harum magni `,
-      author: "Jean B",
-      location: "Montpellier"
-    },
-    {
-      id: 3,
-      text: ` amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit, sit?`,
-      author: "Roberta V",
-      location: "Vosges"
-    },
-    {
-      id: 4,
-      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia !`,
-      author: "James G",
-      location: "Marseille"
-    },
-    {
-      id: 5,
-      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis ?`,
-      author: "Ray B",
-      location: "Paris"
-    },
-    {
-      id: 5,
-      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit
-    excepturi ipsa harum magni labore porro voluptates quibusdam tempora
-    quas sequi animi? Accusantium hic laboriosam provident quaerat ullam
-    soluta reiciendis libero sit nobis? Quasi, sit?`,
-      author: "Ginny T",
-      location: "Brunswick,ME"
-    }
-  ];
+  const TestimonialContext = React.createContext(Testi.slides);
+  const slides = React.useContext(TestimonialContext);
   const [current, setCurrent] = React.useState(0);
   const { length } = slides;
 
@@ -54,25 +11,27 @@ const Testimonial = () => {
   };
 
   React.useEffect(() => {
-    setTimeout(gotoNext, 5000);
+    setTimeout(gotoNext, 3000);
   });
 
   return (
-    <div id="testimonial">
-      <h1>TESTIMONIALS</h1>
-      {slides.map((slide, index) => (
-        <div
-          className={`testimonial-text ${
-            index === current ? "slide-active" : "slide"
-          }`}
-        >
-          <p>{slide.text}</p>
-          <br />
-          <strong>{slide.author} - </strong>
-          <small>{slide.location}</small>
-        </div>
-      ))}
-    </div>
+    <TestimonialContext.Provider value={slides}>
+      <div id="testimonial">
+        <h1>TESTIMONIALS</h1>
+        {slides.map((slide, index) => (
+          <div
+            className={`testimonial-text ${
+              index === current ? "slide-active" : "slide"
+            }`}
+          >
+            <p>{slide.text}</p>
+            <br />
+            <strong>{slide.author} - </strong>
+            <small>{slide.location}</small>
+          </div>
+        ))}
+      </div>
+    </TestimonialContext.Provider>
   );
 };
 
---
+++ src/Context/TestimonialContext.js
@@ -0,0 +1,47 @@
+export const Testi = {
+  slides: [
+    {
+      id: 1,
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit
+    excepturi ipsa harum magni labore porro voluptates quibusdam tempora
+    quas sequi animi? Accusantium hic laboriosam provident quaerat ullam
+    soluta reiciendis libero sit nobis? Quasi, sit?`,
+      author: "Robert T",
+      location: "Jura"
+    },
+    {
+      id: 2,
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit
+    excepturi ipsa harum magni `,
+      author: "Jean B",
+      location: "Montpellier"
+    },
+    {
+      id: 3,
+      text: ` amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit, sit?`,
+      author: "Roberta V",
+      location: "Vosges"
+    },
+    {
+      id: 4,
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia !`,
+      author: "James G",
+      location: "Marseille"
+    },
+    {
+      id: 5,
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis ?`,
+      author: "Ray B",
+      location: "Paris"
+    },
+    {
+      id: 5,
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.Repellatculpa magnam corporis doloremque minima nemo dolorum, id totamvoluptas beatae mollitia dignissimos necessitatibus sed fugit
+    excepturi ipsa harum magni labore porro voluptates quibusdam tempora
+    quas sequi animi? Accusantium hic laboriosam provident quaerat ullam
+    soluta reiciendis libero sit nobis? Quasi, sit?`,
+      author: "Ginny T",
+      location: "Brunswick,ME"
+    }
+  ]
+};
