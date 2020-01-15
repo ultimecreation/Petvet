dc59b70dedf0a1d40ae91ca70472599770ae07b1
ultimecreation <naserbechata@gmail.com>
2020-01-15 02:41:51 +0100

    media queries done

--- src/Components/Adoption.js
+++ src/Components/Adoption.js
@@ -6,8 +6,13 @@ const Adoption = () => {
       <img src={`${process.env.PUBLIC_URL}/img/petits-chiens.jpg`} alt="" />
       <div className="adoption-card">
         <small>TRUE FRIEND</small>
-        <h1>SAVE A LIFE, ADOPT A PET</h1>
+        <h1>
+          SAVE A LIFE,
+          <br />
+          ADOPT A PET
+        </h1>
         <p>Call today to schedule an appointement for your pet</p>
+        <button>READ MORE</button>
       </div>
     </div>
   );
